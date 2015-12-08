77c34c03b4b283beeb02269f649cfd502d5ab72b
owner <owner@192.168.1.169>
2015-12-08 15:42:44 +0900

    クラスパス修正

--- dast.launch/src/dast/internal/launch/DastVMDebugger.java
+++ dast.launch/src/dast/internal/launch/DastVMDebugger.java
@@ -1,9 +1,11 @@
 package dast.internal.launch;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.jdt.internal.launching.StandardVMDebugger;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.VMRunnerConfiguration;
 
@@ -32,8 +34,16 @@ public class DastVMDebugger extends StandardVMDebugger{
 	    }
 	    final IDastLaunchFactory factory = LaunchFactory.createFactory(launch.getLaunchConfiguration());
 	    final IDastProject project = factory.createDastProject(launch);
+	    String projectName = null;
+	    try {
+			projectName = launch.getLaunchConfiguration().getWorkingCopy()
+				  .getAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, "");
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	    return DastDebugPlugin.createDebugTarget(launch, vm,
 	        renderDebugTarget(config.getClassToLaunch(), port), process, true, false,
-	        config.isResumeOnStartup(), project);
+	        config.isResumeOnStartup(), project, projectName);
 	  }
 }
